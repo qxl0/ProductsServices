e2e624f52034d029496b7f3fc63d45ef4ce7e380
Qiang <qxl0@yahoo.com>
2021-12-05 19:56:41 -0500

    Error handling added

--- src/main/java/com/qiang/ProductsServices/ProductsServicesApplication.java
+++ src/main/java/com/qiang/ProductsServices/ProductsServicesApplication.java
@@ -1,5 +1,8 @@
 package com.qiang.ProductsServices;
 
+import com.qiang.ProductsServices.command.api.events.ProductServiceEventsErrorHandler;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +12,11 @@ public class ProductsServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServicesApplication.class, args);
 	}
-
+	@Autowired
+	public void configure(EventProcessingConfigurer configurer){
+		configurer.registerListenerInvocationErrorHandler(
+				"product",
+				configuration -> new ProductServiceEventsErrorHandler()
+		);
+	}
 }
--- src/main/java/com/qiang/ProductsServices/command/api/events/ProductEventsHandler.java
+++ src/main/java/com/qiang/ProductsServices/command/api/events/ProductEventsHandler.java
@@ -2,11 +2,14 @@ package com.qiang.ProductsServices.command.api.events;
 
 import com.qiang.ProductsServices.data.Product;
 import com.qiang.ProductsServices.data.ProductRespository;
+import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@ProcessingGroup("product")
 public class ProductEventsHandler {
     private ProductRespository productRespository;
 
@@ -15,10 +18,16 @@ public class ProductEventsHandler {
     }
 
     @EventHandler
-    public void on(ProductCreatedEvent event){
+    public void on(ProductCreatedEvent event) throws Exception {
         Product product = new Product();
 
         BeanUtils.copyProperties(event, product);
         productRespository.save(product);
+        //throw new Exception("error occured");
+    }
+
+    @ExceptionHandler
+    public void handle(Exception exception) throws Exception {
+        throw exception;
     }
 }
